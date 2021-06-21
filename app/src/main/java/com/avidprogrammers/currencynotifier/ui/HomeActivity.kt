85a1678a4457471c0afdbfc8dec63962180d2044
Abhishek D S <abhishekds94@gmail.com>
2021-06-20 22:24:56 -0400

    update fragment container to androidx component

--- app/src/main/java/com/avidprogrammers/currencynotifier/ui/HomeActivity.kt
+++ app/src/main/java/com/avidprogrammers/currencynotifier/ui/HomeActivity.kt
@@ -2,8 +2,7 @@ package com.avidprogrammers.currencynotifier.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.avidprogrammers.currencynotifier.R
@@ -11,13 +10,13 @@ import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
         //setup bottom nav bar
         bottom_nav.setupWithNavController(navController)
