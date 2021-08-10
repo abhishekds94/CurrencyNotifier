f728cc1ad6e63508991b74274248734bb6963688
Abhishek D S <13012026+abhishekds94@users.noreply.github.com>
2021-08-10 19:48:08 -0400

    Update AppOpen to show after splash screen

--- app/src/main/java/com/avidprogrammers/currencynotifier/ui/MainActivity.kt
+++ app/src/main/java/com/avidprogrammers/currencynotifier/ui/MainActivity.kt
@@ -6,8 +6,12 @@ import android.os.Handler
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.avidprogrammers.currencynotifier.R
+import com.avidprogrammers.currencynotifier.data.forex.ForexValue
+import com.avidprogrammers.currencynotifier.ui.ads.AppOpenManager
 
 class MainActivity : AppCompatActivity() {
+
+    var appOpenManager: AppOpenManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +22,13 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        appOpenManager = AppOpenManager(applicationContext as ForexValue)
+
         Handler().postDelayed({
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
+            appOpenManager!!.showAdIfAvailable()
         }, 3000)
     }
 }
--- app/src/main/java/com/avidprogrammers/currencynotifier/ui/ads/AdmobAppOpenManager.kt
+++ app/src/main/java/com/avidprogrammers/currencynotifier/ui/ads/AdmobAppOpenManager.kt
@@ -1,6 +1,7 @@
 package com.avidprogrammers.currencynotifier.ui.ads
 
 import android.app.Activity
+import android.app.Application
 import android.app.Application.ActivityLifecycleCallbacks
 import android.os.Bundle
 import android.util.Log
@@ -8,7 +9,6 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ProcessLifecycleOwner
-import com.avidprogrammers.currencynotifier.BuildConfig
 import com.avidprogrammers.currencynotifier.data.forex.ForexValue
 import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
@@ -16,23 +16,64 @@ import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.appopen.AppOpenAd
 import com.google.android.gms.ads.appopen.AppOpenAd.AppOpenAdLoadCallback
-import java.util.*
 
 
-class AppOpenManager(myApplication: ForexValue) : LifecycleObserver,
-    ActivityLifecycleCallbacks {
-    private val myApplication: ForexValue = myApplication
+/**
+ * Prefetches App Open Ads.
+ */
+class AdmobAppOpenManager(private val myApplication: ForexValue) :
+    ActivityLifecycleCallbacks, LifecycleObserver {
     private var appOpenAd: AppOpenAd? = null
     private var loadCallback: AppOpenAdLoadCallback? = null
     private var currentActivity: Activity? = null
-    private var loadTime: Long = 0
 
+    /**
+     * Request an ad
+     */
+    fun fetchAd() {
+        // Have unused ad, no need to fetch another.
+        if (isAdAvailable) {
+            return
+        }
+        loadCallback = object : AppOpenAdLoadCallback() {
+            /**
+             * Called when an app open ad has loaded.
+             *
+             * @param ad the loaded app open ad.
+             */
+            fun onAppOpenAdLoaded(ad: AppOpenAd?) {
+                appOpenAd = ad
+            }
+
+            /**
+             * Called when an app open ad has failed to load.
+             *
+             * @param loadAdError the error.
+             */
+            fun onAppOpenAdFailedToLoad(loadAdError: LoadAdError?) {
+                // Handle the error.
+                Log.d(LOG_TAG, "error in loading")
+            }
+        }
+        val request = adRequest
+        AppOpenAd.load(
+            myApplication, AD_UNIT_ID, request,
+            AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT, loadCallback
+        )
+    }
+
+    /**
+     * Shows the ad if one isn't already showing.
+     */
     fun showAdIfAvailable() {
+        // Only show ad if there is not already an app open ad currently showing
+        // and an ad is available.
         if (!isShowingAd && isAdAvailable) {
             Log.d(LOG_TAG, "Will show ad.")
             val fullScreenContentCallback: FullScreenContentCallback =
                 object : FullScreenContentCallback() {
                     override fun onAdDismissedFullScreenContent() {
+                        // Set the reference to null so isAdAvailable() returns false.
                         appOpenAd = null
                         isShowingAd = false
                         fetchAd()
@@ -43,7 +84,6 @@ class AppOpenManager(myApplication: ForexValue) : LifecycleObserver,
                         isShowingAd = true
                     }
                 }
-            appOpenAd!!.fullScreenContentCallback = fullScreenContentCallback
             appOpenAd!!.show(currentActivity)
         } else {
             Log.d(LOG_TAG, "Can not show ad.")
@@ -51,54 +91,19 @@ class AppOpenManager(myApplication: ForexValue) : LifecycleObserver,
         }
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
-    fun onStart() {
-        showAdIfAvailable()
-        Log.d(LOG_TAG, "onStart")
-    }
-
-    fun fetchAd() {
-        if (isAdAvailable) {
-            return
-        }
-        loadCallback = object : AppOpenAdLoadCallback() {
-
-            override fun onAdLoaded(appOpenAd: AppOpenAd) {
-                super.onAdLoaded(appOpenAd)
-                this@AppOpenManager.appOpenAd = appOpenAd
-                loadTime = Date().time
-                Log.d(LOG_TAG, "App-Open Ad Loaded")
-            }
-
-            override fun onAdFailedToLoad(loadAdError: LoadAdError) {
-                super.onAdFailedToLoad(loadAdError)
-                fetchAd()
-            }
-        }
-
-        val request = adRequest
-        AppOpenAd.load(
-            myApplication,
-            AD_UNIT_ID,
-            request,
-            AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT,
-            loadCallback!!
-        )
-    }
-
+    /**
+     * Creates and returns ad request.
+     */
     private val adRequest: AdRequest
-        get() = AdRequest.Builder().build()
+        private get() = AdRequest.Builder().build()
 
+    /**
+     * Utility method that checks if ad exists and can be shown.
+     */
     val isAdAvailable: Boolean
-        get() = appOpenAd != null && wasLoadTimeLessThanNHoursAgo()
+        get() = appOpenAd != null
 
-    private fun wasLoadTimeLessThanNHoursAgo(): Boolean {
-        val dateDifference = Date().time - loadTime
-        val numMilliSecondsPerHour: Long = 3600000
-        return dateDifference < numMilliSecondsPerHour * 4.toLong()
-    }
-
-    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+    override fun onActivityCreated(activity: Activity, bundle: Bundle?) {}
     override fun onActivityStarted(activity: Activity) {
         currentActivity = activity
     }
@@ -109,19 +114,29 @@ class AppOpenManager(myApplication: ForexValue) : LifecycleObserver,
 
     override fun onActivityPaused(activity: Activity) {}
     override fun onActivityStopped(activity: Activity) {}
-    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+    override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {}
     override fun onActivityDestroyed(activity: Activity) {
         currentActivity = null
     }
 
+    /** LifecycleObserver methods  */
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStart() {
+        showAdIfAvailable()
+        Log.d(LOG_TAG, "onStart")
+    }
+
     companion object {
         private const val LOG_TAG = "AppOpenManager"
-        private const val AD_UNIT_ID = BuildConfig.ADMOB_APPOPEN
+        private const val AD_UNIT_ID = "ca-app-pub-68959573532223023/3473201416"
         private var isShowingAd = false
     }
 
+    /**
+     * Constructor
+     */
     init {
-        this.myApplication.registerActivityLifecycleCallbacks(this)
+        myApplication.registerActivityLifecycleCallbacks(this)
         ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
 }
